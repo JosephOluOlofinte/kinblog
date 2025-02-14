94d28704d86cc6dfd14e3a8d38b6c0a151825a5c
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-02-14 21:36:30 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,37 @@ import { posts } from "./posts.js";
 // main.js
 document.addEventListener('DOMContentLoaded', () => {
     const postList = document.getElementById('post-list');
+    const postTitle = document.getElementById('post-title');
+    const postImg = document.getElementById('post-img');
+    const postSum = document.getElementById('post-sum');
+    const postLink = document.getElementById('post-link');
   
     // Loop through the posts array and create a summary for each post
-    posts.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.classList.add('post');
+    // posts.forEach(post => {
+    //   const postElement = document.createElement('div');
+    //   postElement.className = "post";
   
-      postElement.innerHTML = `
+    //   postElement.innerHTML = `
+    //     <h2>${post.title}</h2>
+    //     <img src="${post.coverImage}" alt="${post.title}">
+    //     <p>${post.summary}</p>
+    //     <a href="full-post.html?id=${post.id}">Read More</a>
+    //   `;
+
+    postList.innerHTML = posts.map(post => `
+      <div class="post-box">
         <h2>${post.title}</h2>
         <img src="${post.coverImage}" alt="${post.title}">
         <p>${post.summary}</p>
-        <a href="full-post.html?id=${post.id}">Read More</a>
-      `;
+        <div class="button-box">
+          <a href="full-post.html?id=${post.id}">Read More</a>
+        </div>  
+      </div>
+      `)
+
+
   
-      postList.appendChild(postElement);
-    });
+    //   postList.appendChild(postElement);
+    // });
   });
   
